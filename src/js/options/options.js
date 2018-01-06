8de16750b90b59203cd222f52120b3a878a46d5f
Benjamin Huang <ben.huang06@gmail.com>
2018-01-06 02:10:39 -0500

    Options manager to link to update options page

---
+++ src/js/options/options.js
@@ -0,0 +1,42 @@
+import storage from "./storage";
+import defaultProperties from "./propertyDefaults";
+
+const syncSettings = document.querySelector("#sync-settings");
+const fontFamily = document.querySelector("#view-font-family");
+const fontSize = document.querySelector("#view-font-size");
+const lineHeight = document.querySelector("#view-line-height");
+const saveButton = document.querySelector("#save-settings");
+const resetButton = document.querySelector("#reset-settings");
+
+// Once called, return the current state of the values of the
+// settings in the UI.
+const propertyState = () => {
+    return {
+        syncSettings: syncSettings.checked,
+        lineHeight: lineHeight.value,
+        fontFamily: fontFamily.value,
+        fontSize: fontSize.value
+    };
+};
+
+const loadProperties = props => {
+    storage.getProperties(props).then(loaded => {
+        syncSettings.checked = loaded.syncSettings;
+        lineHeight.value = loaded.lineHeight;
+        fontFamily.value = loaded.fontFamily;
+        fontSize.value = loaded.fontSize;
+    });
+};
+
+const setProperties = props => {
+    // Refresh UI once properties are loaded.
+    storage.setProperties(props, () => loadProperties(defaultProperties));
+};
+
+// On init, restore properties using the storage API.
+loadProperties(defaultProperties);
+
+// Add event listeners
+syncSettings.addEventListener("click", () => storage.setProperty("syncSettings", syncSettings.checked));
+saveButton.addEventListener("click", () => setProperties(propertyState()));
+resetButton.addEventListener("click", () => setProperties(defaultProperties));
