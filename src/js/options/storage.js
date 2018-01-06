86f68442a942573e5dc5813a71a42dc7996ff0d7
Benjamin Huang <ben.huang06@gmail.com>
2018-01-06 02:08:46 -0500

    Provide callback to property functions

--- src/js/options/storage.js
+++ src/js/options/storage.js
@@ -17,41 +17,57 @@ class StorageManager {
         return this.storage;
     }
 
-    getProperty(property) {
+    getProperty(property, callback) {
         return new Promise((resolve, reject) => {
             this.storage.get(property, opt => {
-                if (opt)
+                if (opt) {
+                    if (callback)
+                        callback();
+
                     resolve(opt[property]);
+                }
+                
                 else reject(new Error("Unable to load property."));
             });
         });
     }
 
-    setProperty(property, value) {
+    setProperty(property, value, callback) {
         return new Promise((resolve, reject) => {
             const store = {};
             store[property] = value;
 
             this.storage.set(store, () => {
+                if (callback)
+                    callback();
+
                 resolve(true);
             });
         });
     }
 
-    getProperties(properties) {
+    getProperties(properties, callback) {
         // Return a Promise that resolves once we have the loaded props.
         return new Promise((resolve, reject) => {
             this.storage.get(properties, opts => {
-                if (opts)
+                if (opts) {
+                    if (callback)
+                        callback();
+                        
                     resolve(opts);
+                }
+
                 else reject(new Error("Unabled to load properties. Please submit an issue."));
             });
         });
     }
 
-    setProperties(properties) {
+    setProperties(properties, callback) {
         return new Promise((resolve, reject) => {
             this.storage.set(properties, () => {
+                if (callback)
+                    callback();
+
                 resolve(true);
             });
         });
