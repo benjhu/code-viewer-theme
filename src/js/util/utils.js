9ee7cf4ef9525d243da82126e67f2c13ccf6be9d
Benjamin Huang <ben.huang06@gmail.com>
2018-01-09 20:13:31 -0500

    URL matcher

--- src/js/util/utils.js
+++ src/js/util/utils.js
@@ -17,3 +17,33 @@ export const toConsole = (message, warn = false) => {
         fn(message);
     }
 };
+
+export const matchUrl = (url, matchPattern, matchHttp = true, exactMatch = false) => {
+    const a = url.split("/");
+    const b = matchPattern.split("/");
+
+    if (exactMatch && (a.length !== b.length))
+        return false;
+
+    if (a[0] !== "https:" && a[0] !== "http:")
+        return false;
+
+    if (b[0] !== "https:" && b[0] !== "http:")
+        return false;
+
+    if (matchHttp && (a[0] !== b[0]))
+        return false;
+
+    for (let i = 1; i < a.length; i++) {
+        if (!exactMatch && i == b.length - 1 && b[i] === "*")
+            return true;
+
+        if (b[i] === "*")
+            continue;
+
+        if (a[i] !== b[i])
+            return false;
+    }
+
+    return true;
+};
