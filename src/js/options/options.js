f0e70b2faeabd04784d430408f29cfbc63bb6e50
Benjamin Huang <ben.huang06@gmail.com>
2018-01-19 15:51:27 -0500

    Dynamically create refs to DOM inputs for options

--- src/js/options/options.js
+++ src/js/options/options.js
@@ -1,24 +1,69 @@
 import storage from "../util/storage";
 import defaultProperties from "./propertyDefaults";
 import { dialog } from "./materialize.scripts";
+import propertySets from "../extension/property-sets/exports";
 
-const syncSettings = document.querySelector("#sync-settings");
-const fontFamily = document.querySelector("#view-font-family");
-const fontSize = document.querySelector("#view-font-size");
-const lineHeight = document.querySelector("#view-line-height");
-const saveButton = document.querySelector("#save-settings");
-const resetButton = document.querySelector("#reset-settings");
+const qs = window.document.querySelector.bind(document);
+
+// GENERAL
+const syncSettings = qs("#sync-settings");
+const fontFamily = qs("#view-font-family");
+const fontSize = qs("#view-font-size");
+const lineHeight = qs("#view-line-height");
+const saveButton = qs("#save-settings");
+const resetButton = qs("#reset-settings");
+
+const propertySetInputNodes = {};
+
+// Automatically create references to the DOM nodes that supply the option
+// value.
+propertySets.forEach(propertySet => {
+    const name = propertySet.name.toLowerCase();
+
+    propertySetInputNodes[name] = {};
+    const elements = propertySetInputNodes[name];
+    const props = propertySet.properties;
+
+    Object.keys(props).forEach(key => {
+        elements[key] = {
+            input: qs(`#${name}-${props[key].css}`),
+        };
+
+        if (props[key].inheritable)
+            elements[key].inheritFrom = qs(`#view-${props[key].css}`);
+
+        if (props[key].binary)
+            elements[key].binary = true;
+    });
+});
+
+console.log(propertySetInputNodes);
 
 // Once called, return the current state of the values of the
 // settings in the UI.
 const propertyState = () => {
-    return {
+    const state = {
         syncSettings: syncSettings.checked,
         lineHeight: lineHeight.value,
         fontFamily: fontFamily.value,
         fontSize: fontSize.value
     };
+
+    Object.keys(propertySetInputNodes).forEach(set => {
+        const stateOfSet = state[set] = {};
+        const props = propertySetInputNodes[set];
+
+        Object.keys(props).forEach(key => {
+            const value = props[key];
+            stateOfSet[key] = props.binary ? value.input.checked : value.input.value;
+        });
+    });
+
+    return state;
 };
+
+// Todo: delete
+window.__propState = propertyState;
 
 const loadProperties = props => {
     storage.getProperties(props).then(loaded => {
