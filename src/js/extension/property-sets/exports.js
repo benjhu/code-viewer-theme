120c6a36f271e1628585f5d1cc32b7632fd86de6
Benjamin Huang <ben.huang06@gmail.com>
2018-01-21 17:01:40 -0500

    Fallback method for obtaining default values

--- src/js/extension/property-sets/exports.js
+++ src/js/extension/property-sets/exports.js
@@ -1,5 +1,22 @@
 import * as githubPropertySet from "./github";
 
+/**
+ * Returns an object containing the key-value mappings of a {@code propertySet}'s properties and their default values.
+ *
+ * @param {object} propertySet a property set.
+ * @returns {object} all properties and their default values.
+ */
+export const getDefaultProperties = propertySet => {
+    const defaults = {};
+    const keys = Object.keys(propertySet.properties);
+
+    keys.forEach(key => {
+        defaults[key] = propertySet[key].defaultValue;
+    });
+
+    return defaults;
+};
+
 // Export the property sets to be generated into CSS declarations.
 export default [
     githubPropertySet
