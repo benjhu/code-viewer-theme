df27283310a278c87aef9bbc1247afecd792aa8e
Benjamin Huang <ben.huang06@gmail.com>
2018-01-25 21:46:54 -0500

    Redux store for UI

---
+++ src/js/shared/reducers.js
@@ -0,0 +1,9 @@
+import optionsReducer from "./optionsReducer";
+
+/**
+ * All reducers for the web application portion of the extension.
+ * Actions dispatched to the store should have types prefixed with the corresponding
+ * reducer.
+ */
+const reducers = { optionsReducer };
+export default reducers;
---
+++ src/js/shared/store.js
@@ -0,0 +1,8 @@
+import Redux from "redux";
+import reducers from "./reducers";
+
+const store = Redux.createStore(
+    Redux.combineReducers(reducers)
+);
+
+export default store;
