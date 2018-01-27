e80e0433a2802e280c5db4a06418d045d5610fa3
Benjamin Huang <ben.huang06@gmail.com>
2018-01-27 00:49:46 -0500

    Add reducer for sets

--- src/js/shared/reducers.js
+++ src/js/shared/reducers.js
@@ -1,9 +1,20 @@
 import optionsReducer from "./optionsReducer";
 
+const arrayOfReducers = [optionsReducer];
+
 /**
  * All reducers for the web application portion of the extension.
  * Actions dispatched to the store should have types prefixed with the corresponding
  * reducer.
  */
-const reducers = { optionsReducer };
-export default reducers;
+const reducers = allReducers => {
+    const r = {};
+
+    allReducers.forEach(reducer => {
+        r[reducer.name] = reducer.reducer;
+    });
+
+    return r;
+};
+
+export default reducers(arrayOfReducers);
---
+++ src/js/shared/setsReducer.js
@@ -0,0 +1,66 @@
+export const name = "propertySets";
+
+/**
+ * Available actions for this reducer.
+ */
+export const actions = {
+    ADD_SET: "SETS_ADD_SET",
+    DELETE_SET: "SETS_DELETE_SET",
+    AMEND_SET: "SETS_AMEND_SET"
+};
+
+/**
+ * Returns the next state after amending a new set.
+ *
+ * @param {object} state state of available state
+ * @param {object} setInfo information about the new set
+ * @param {boolean} amend determines how the incoming set is treated
+ */
+const newSet = (state, setInfo, amend = false) => {
+    // If set already exists, do nothing.
+    // Or, if we try to amend when there is no set.
+    if ((!amend && state[setInfo.id]) || (amend && !state[setInfo.id]))
+        return state;
+
+    return {
+        ...state,
+        nextID: setInfo.id + 1,
+        sets: {
+            ...state.sets,
+            [state.nextID]: {
+                ...setInfo
+            }
+        }
+    };
+};
+
+/**
+ * A "sub-reducer" that updates the object containing the ID-property set pairs.
+ * Accepts a an action formatted with:
+ *  - setName: string name of incoming set
+ *
+ * @param {object} state the state of the Redux store
+ * @param {object} action an action dispatched to the Redux store
+ */
+export const reducer = (state = { sets: {} }, action) => {
+    switch (action.type) {
+        case actions.ADD_SET:
+            return newSet(state, {
+                id: state.nextID,
+                name: action.setName
+            });
+
+        case actions.AMEND_SET:
+            return newSet(state, {
+                id: state.nextID,
+                name: action.setName
+            }, true);
+
+        default:
+            return state;
+    }
+};
+
+export default {
+    name, actions, reducer
+};
